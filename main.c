f4a6943a30a1baf0493ff6c924439a863d50a142
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-06-24 20:01:42 +0200

    new

--- main.c
+++ main.c
@@ -14,28 +14,34 @@ int clicked(Vector2 mouse_pos, Rectangle button) {
 	else if (CheckCollisionPointRec(mouse_pos, button) && IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) return 2;
 }
 
-float isochoric(bool check[], float temp, float vol, float press, float dtemp) {
-	if (check[0] && check[1]) {
+float isochoric(bool change, float temp, float vol, float press, float dtemp) {
+	if (change) {
 		DrawText("ISOCHORIC TRANSFORMATION", 10,770,20,WHITE);
 		return (temp/(temp-dtemp))*press;
 	}
 	return press;
 }
 
-float isothermic(bool check[], float temp, float vol, float press, float dvol) {
-	if (check[0] && check[1]) {
+float isothermic(bool change, float temp, float vol, float press, float dvol) {
+	if (change) {
 		DrawText("ISOTHERMIC TRANSFORMATION", 10,770,20,WHITE);
 		return ((vol+dvol)/vol)*press;
 	}
 	return press;
 }
 
-float isobar(bool check[], float temp, float vol, float press, float dtemp) {
-	if (check[0] && check[1]) {
+float isobar(bool change, float temp, float vol, float press, float dtemp) {
+	if (change) {
 		DrawText("ISOBAR TRANSFORMATION", 10,770,20,WHITE);
 		return (temp/(temp-dtemp))*vol;
 	}
 	return vol;
+}
+
+void debug(bool blocked, bool compress, bool heat, bool isIsothermic, bool isIsochoric, bool isIsobar) {
+	printf("1true:  blocked:%d -- compress %d -- heat %d\n", blocked, compress, heat);
+	printf("1true:  isotherm:%d -- isochoric %d -- isbar %d\n", isIsothermic, isIsochoric, isIsobar);
+	printf("\n");
 }
 
 
@@ -48,7 +54,6 @@ int main() {
 	float temp = 220.0;
 	float press = 1.01325e5;
 	float vol = 1;
-	float area = 2;
 
 	int MX = GetMouseX();
 	int MY = GetMouseY();
@@ -56,13 +61,11 @@ int main() {
 	// Buttons color
 	Color button_col[] = {RED, RED, RED};
 
-	bool draw_flame = false;
 	float vel = 0.0f;
 
-	// Temp, press, vol
-	bool check_isochoric[] = {false, false};
-	bool check_isotherm[] = {false, true};
-	bool check_isobar[] = {false, false};
+	bool blocked = false;
+	bool compress = false;
+	bool heat = false;
 
 	InitWindow(X,Y, "Piston");
 	SetTargetFPS(60);
@@ -80,17 +83,20 @@ int main() {
 		BeginDrawing();
 		ClearBackground(GRAY);
 
-		float h = vol / area;
 		float dvol = vel/267;
 		float dtemp = 0.5/26.7;
 
-		/* printf("pist %f, centr %f\n", piston.y+0.4, central_y - 0.2); */
-		if (piston.y < central_y + depth - 10 && piston.y > central_y - 0.2) {
+		if (piston.y < central_y + depth - 10) {
 			piston.y += vel;
 			vol -= dvol;
 		}
 
-		if (draw_flame) {
+		if (piston.y <= central_y - 0.2) {
+			vel = 0.f;
+			vol = 1.f;
+		} 
+
+		if (heat) {
 			DrawTexture(flame_tex, X/2-10, central_y + depth + height + 20, WHITE); // Flame
 			DrawText("FLAME", X/2-30, central_y + depth + height + 120, 20,WHITE);
 			if (temp <= 500.f) temp += dtemp;
@@ -107,18 +113,21 @@ int main() {
 		DrawRectangleRounded(piston, 0.4, 10, WHITE);
 		DrawRectangleLinesEx(cilinder, 4, RED);
 
-		if (check_isotherm[0] && check_isotherm[1]) {
-			press = isothermic(check_isotherm, temp, vol, press, dvol);
-		} if (check_isochoric[0] && check_isochoric[1]) {
-			press = isochoric(check_isochoric, temp, vol, press, dtemp);
-		}
+		bool isIsothermic = (compress && !blocked && !heat);
+		bool isIsochoric = (blocked && heat && !compress);
+		bool isIsobar = (heat && !blocked && !compress);
 
-		/* float changing_vol = isobar(check_isobar, temp, vol, press, dtemp); */
-		/* float dvol1 = -(vol - changing_vol)*2000; */
-		/* if (check_isobar && !check_isotherm) { */
-		/* 	piston.y -= vel; */
-		/* 	vol += dvol; */
-		/* } */
+		/* debug(blocked, compress, heat, isIsothermic, isIsochoric, isIsobar); */
+
+		if (isIsothermic) {
+			press = isothermic(compress, temp, vol, press, dvol);
+		}
+		if (isIsochoric) {
+			press = isochoric(blocked, temp, vol, press, dtemp);
+		} 
+		if (isIsobar) {
+			vol = isobar(heat, temp, vol, press, dtemp);
+		}
 
 		DrawText(TextFormat("Temperature: %.2f K", temp), 10,10,20,WHITE);
 		DrawText(TextFormat("Pressure: %.2f Pa", press), 10,30,20,WHITE);
@@ -131,45 +140,40 @@ int main() {
 
 		Rectangle button0 = {945, 12, 15, 15};
 
-		bool check_no_transf = (check_isotherm[0] && check_isochoric[1]);
-
-		if (clicked(mouse_pos, button0) == 1 || check_no_transf) {
+		if (clicked(mouse_pos, button0) == 1 && !compress) {
 			button_col[0] = GREEN;
 			button_col[2] = RED;
 			vel = 0;
-			check_isochoric[0] = true;
-			check_isotherm[0] = false;
-			check_isobar[0] = false;
+			blocked = true;
 		}
-		else if (clicked(mouse_pos, button0) == 2) {
+		else if (clicked(mouse_pos, button0) == 2 && !compress) {
 			button_col[0] = RED;
-			check_isochoric[0] = false;
-			check_isobar[0] = true;
+			if (isIsobar) vel = -0.2f;
+			else if (isIsothermic) vel = 0.2f;
+			blocked = false;
 		}
 
 		DrawRectangleRounded(button0, 0.8, 40, button_col[0]);
-
 
 		// Button to heat up the gas
 		DrawText("Heat the gas:", X-270,30,20,WHITE);
 
 		Rectangle button1 = {945, 32, 15, 15};
 
-		if (clicked(mouse_pos, button1) == 1) {
+		/* printf("check %d, isobar %d\n", heat && !blocked && !compress, isIsobar); */
+		printf("vel %f\n", vel);
+		if (clicked(mouse_pos, button1) == 1 && !compress) {
 			button_col[1] = GREEN;
-			draw_flame = true;
-			check_isochoric[1] = true;
-			check_isotherm[1] = false;
-			check_isobar[1] = true;
-			if (check_isobar && !check_isochoric[0]) vel = -0.2f;
+			heat = true;
+			if (heat && !blocked && !compress) {
+				vel = -0.2f;
+			}
 		}
-		else if (clicked(mouse_pos, button1) == 2) {
+		else if (clicked(mouse_pos, button1) == 2 && !compress) {
 			button_col[1] = RED;
-			draw_flame = false;
-			check_isochoric[1] = false;
-			check_isotherm[1] = true;
-			check_isobar[1] = false;
-			if (check_isobar) vel = 0.f;
+			heat = false;
+			/* if (!heat || blocked || compress) vel = 0.f; */
+			if (!heat) vel = 0.f;
 		}
 
 		DrawRectangleRounded(button1, 0.8, 40, button_col[1]);
@@ -179,19 +183,16 @@ int main() {
 
 		Rectangle button2 = {945, 55, 15, 15};
 
-		if (clicked(mouse_pos, button2) == 1) {
+		if (clicked(mouse_pos, button2) == 1 && !blocked && !heat) {
 			button_col[2] = GREEN;
 			button_col[0] = RED;
 			vel = 0.2f;
-			check_isochoric[0] = false;
-			check_isotherm[0] = true;
-			check_isobar[0] = false;
+			compress = true;
 		}
-		else if (clicked(mouse_pos, button2) == 2) {
+		else if (clicked(mouse_pos, button2) == 2 && !blocked && !heat) {
 			button_col[2] = RED;
-			vel = 0;
-			check_isotherm[0] = false;
-			check_isobar[0] = true;
+			vel = 0.f;
+			compress = false;
 		}
 
 		DrawRectangleRounded(button2, 0.8, 40, button_col[2]);
